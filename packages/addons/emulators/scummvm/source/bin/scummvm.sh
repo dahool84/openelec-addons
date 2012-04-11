7d4c1a1b6e71bd2892e9b8887a9cd97119b9be70
Matej Zevnik <mzevnik@mzevniklap-VirtualBox.(none)>
2012-04-11 09:23:36 +0200

    first commit

---
+++ packages/addons/emulators/scummvm/source/bin/scummvm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+ADDON_DIR="$HOME/.xbmc/addons/emulators.scummvm"
+ADDON_HOME="$HOME/.xbmc/userdata/addon_data/emulators.scummvm"
+
+mkdir -p "/storage/emulators/scummvm/roms"
+
+export LD_LIBRARY_PATH=$ADDON_DIR/bin/
+
+chmod a+rx "$ADDON_DIR/bin/scummvm"
+"$ADDON_DIR/bin/scummvm" -f
+
+sleep 7
+killall -STOP xbmc.bin
+
+while [ $(pidof scummvm) ];do
+usleep 200000
+done;
+killall -CONT xbmc.bin
