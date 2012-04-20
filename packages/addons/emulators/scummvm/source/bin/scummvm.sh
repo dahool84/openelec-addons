298682a0d8cb8e98e41e2ce994daae774a776deb
Matej Zevnik <matej.zevnik@innbox.net>
2012-04-20 14:02:12 +0200

    beta version

---
+++ packages/addons/emulators/scummvm/source/bin/scummvm.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+ADDON_DIR="$HOME/.xbmc/addons/emulators.scummvm"
+ADDON_HOME="$HOME/.xbmc/userdata/addon_data/emulators.scummvm"
+
+mkdir -p "/storage/emulators/scummvm/roms"
+
+export LD_LIBRARY_PATH=$ADDON_DIR/bin/
+
+chmod a+rx "$ADDON_DIR/bin/scummvm"
+
+$ADDON_DIR/bin/scummvm --themepath=$ADDON_DIR/bin/
+
+
+
+
+
+
+
+
+
+
+
